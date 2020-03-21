61e773f5276bf99bbeb9d38dd145b4b5992b6ed2
Luca Effgen <l.effgen@datamints.com>
2020-03-21 16:42:21 +0100

    fsdgh

--- src/store/modules/ToDos.js
+++ src/store/modules/ToDos.js
@@ -55,7 +55,7 @@ const actions = {
   async addTodo (context, todo) {
     // Prepare the todo object
     const currentList = state.currentList.listId
-    const todoData = { content: todo, isCompleted: false, isInProgress: false, isInTodo: true, todo_list_id: currentList }
+    const todoData = { content: todo, isCompleted: false, isInProgress: false, isInTodo: true, todoList: { id: currentList } }
     // Send an api call to add the todo
     try {
       const response = await fetch('https://ap-todo-list.herokuapp.com/addTodo', {
